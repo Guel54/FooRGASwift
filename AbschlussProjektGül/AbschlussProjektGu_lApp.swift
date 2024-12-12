c9249f4c45b005be4c8e398c2419cf8f3c3b6ab0
Mavi <gkraif54@gmail.com>
2024-12-12 09:52:41 +0100

    Initial Commit

---
+++ AbschlussProjektGül/AbschlussProjektGu_lApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbschlussProjektGu_lApp.swift
+//  AbschlussProjektGül
+//
+//  Created by Gül Köse on 12.12.24.
+//
+
+import SwiftUI
+
+@main
+struct AbschlussProjektGu_lApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
